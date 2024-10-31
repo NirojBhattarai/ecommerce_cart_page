3c91b10a8498bea9918f6f33e9752f0d14a62e38
NirojBhattarai <neerajbhttri@gmail.com>
2024-10-31 09:22:04 +0545

    Add Function to Delete Items from Cart and Store Cart Elements in Local Storage

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   ];
 
-  let cart = [];
+  //Initializing and Fetching Products Data froo Local Storage
+
+  let cart = JSON.parse(localStorage.getItem("Products")) || [];
 
   const productList = document.getElementById("product-list");
   const cartItems = document.getElementById("cart-items");
@@ -28,7 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const checkOutBtn = document.getElementById("checkout-btn");
 
   //Iterating Products Array and Adding into Product List
-
   products.forEach((product) => {
     const productDiv = document.createElement("div");
     productDiv.classList.add("product");
@@ -45,6 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const productId = parseInt(e.target.getAttribute("data-id"));
       const product = products.find((p) => p.id === productId);
       addToCart(product);
+      saveProduct();
     }
   });
 
@@ -62,12 +64,17 @@ document.addEventListener("DOMContentLoaded", () => {
     if (cart.length > 0) {
       emptyCart.classList.add("hidden");
       cartTotal.classList.remove("hidden");
+
       cart.forEach((item, index) => {
         totalPrice += item.price;
         const cartItem = document.createElement("div");
         cartItem.classList.add("product");
+
         cartItem.innerHTML = `
-        <span> ${item.name} - $${item.price} </span>`;
+        <span> ${item.name} - $${item.price} 
+        <button data-id = ${item.id}>Delete Item </button>
+        </span>`;
+
         cartItems.appendChild(cartItem);
         totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`;
       });
@@ -79,7 +86,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Adding Event Listener to CheckOut Button
   checkOutBtn.addEventListener("click", () => {
-    cart.length = 0;
+    cart = [];
+    saveProduct();
     renderCart();
   });
+
+  // Event Listener to delete item from cart
+  cartItems.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const productId = parseInt(e.target.getAttribute("data-id"));
+      const index = cart.findIndex((item) => item.id === productId);
+      if (index !== -1) {
+        cart.splice(index, 1);
+        saveProduct();
+        renderCart();
+      }
+    }
+  });
+
+  //Function to Store Product in Local Storage
+  function saveProduct() {
+    localStorage.setItem("Products", JSON.stringify(cart));
+  }
+
+  renderCart();
 });
